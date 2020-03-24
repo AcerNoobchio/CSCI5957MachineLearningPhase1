30f708a424856085aafaca787f212fe2d5486c14
Hoyos, Jacob <HOYOSJ@ETSU.EDU>
2020-03-24 03:08:09 -0400

    I made so many changes I don't really know where to start: bottom line: we have a sample rate and we can display a single feature across all files in a plot

--- classifier.py
+++ classifier.py
@@ -42,233 +42,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import datetime as dt
 import glob as files
-
 try:
     # [OPTIONAL] Seaborn makes plots nicer
     import seaborn
 except ImportError:
     pass
-
-# =====================================================================
-
-def download_data():
-    '''
-    Downloads the data for this script into a pandas DataFrame.
-    '''
-
-    # If your data is in an Excel file, install 'xlrd' and use
-    # pandas.read_excel instead of read_table
-    #from pandas import read_excel
-    #frame = read_excel(URL)
-
-    # If your data is in a private Azure blob, install 'azure-storage' and use
-    # BlockBlobService.get_blob_to_path() with read_table() or read_excel()
-    #from azure.storage.blob import BlockBlobService
-    #service = BlockBlobService(ACCOUNT_NAME, ACCOUNT_KEY)
-    #service.get_blob_to_path(container_name, blob_name, 'my_data.csv')
-    #frame = read_table('my_data.csv', ...
-
-    frame = read_table(
-        URL,
-        
-        # Uncomment if the file needs to be decompressed
-        #compression='gzip',
-        #compression='bz2',
-
-        # Specify the file encoding
-        # Latin-1 is common for data from US sources
-        encoding='latin-1',
-        #encoding='utf-8',  # UTF-8 is also common
-
-        # Specify the separator in the data
-        sep=',',            # comma separated values
-        #sep='\t',          # tab separated values
-        #sep=' ',           # space separated values
-
-        # Ignore spaces after the separator
-        skipinitialspace=True,
-
-        # Generate row labels from each row number
-        index_col=None,
-        #index_col=0,       # use the first column as row labels
-        #index_col=-1,      # use the last column as row labels
-
-        # Generate column headers row from each column number
-        header=None,
-        #header=0,          # use the first line as headers
-
-        # Use manual headers and skip the first row in the file
-        #header=0,
-        #names=['col1', 'col2', ...],
-    )
-
-    # Return a subset of the columns
-    #return frame[['col1', 'col4', ...]]
-
-    # Return the entire frame
-    return frame
-
-
-# =====================================================================
-
-
-def get_features_and_labels(frame):
-    '''
-    Transforms and scales the input data and returns numpy arrays for
-    training and testing inputs and targets.
-    '''
-
-    # Replace missing values with 0.0, or we can use
-    # scikit-learn to calculate missing values (below)
-    #frame[frame.isnull()] = 0.0
-
-    # Convert values to floats
-    arr = np.array(frame, dtype=np.float)
-
-    # Use the last column as the target value
-    X, y = arr[:, :-1], arr[:, -1]
-    # To use the first column instead, change the index value
-    #X, y = arr[:, 1:], arr[:, 0]
-    
-    # Use 80% of the data for training; test against the rest
-    from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-    # sklearn.pipeline.make_pipeline could also be used to chain 
-    # processing and classification into a black box, but here we do
-    # them separately.
-    
-    # If values are missing we could impute them from the training data
-    #from sklearn.preprocessing import Imputer
-    #imputer = Imputer(strategy='mean')
-    #imputer.fit(X_train)
-    #X_train = imputer.transform(X_train)
-    #X_test = imputer.transform(X_test)
-    
-    # Normalize the attribute values to mean=0 and variance=1
-    from sklearn.preprocessing import StandardScaler
-    scaler = StandardScaler()
-    # To scale to a specified range, use MinMaxScaler
-    #from sklearn.preprocessing import MinMaxScaler
-    #scaler = MinMaxScaler(feature_range=(0, 1))
-    
-    # Fit the scaler based on the training data, then apply the same
-    # scaling to both training and test sets.
-    scaler.fit(X_train)
-    X_train = scaler.transform(X_train)
-    X_test = scaler.transform(X_test)
-
-    # Return the training and test sets
-    return X_train, X_test, y_train, y_test
-
-
-# =====================================================================
-
-
-def evaluate_classifier(X_train, X_test, y_train, y_test):
-    '''
-    Run multiple times with different classifiers to get an idea of the
-    relative performance of each configuration.
-
-    Returns a sequence of tuples containing:
-        (title, precision, recall)
-    for each learner.
-    '''
-
-    # Import some classifiers to test
-    from sklearn.svm import LinearSVC, NuSVC
-    from sklearn.ensemble import AdaBoostClassifier
-
-    # We will calculate the P-R curve for each classifier
-    from sklearn.metrics import precision_recall_curve, f1_score
-    
-    # Here we create classifiers with default parameters. These need
-    # to be adjusted to obtain optimal performance on your data set.
-    
-    # Test the linear support vector classifier
-    classifier = LinearSVC(C=1)
-    # Fit the classifier
-    classifier.fit(X_train, y_train)
-    score = f1_score(y_test, classifier.predict(X_test))
-    # Generate the P-R curve
-    y_prob = classifier.decision_function(X_test)
-    precision, recall, _ = precision_recall_curve(y_test, y_prob)
-    # Include the score in the title
-    yield 'Linear SVC (F1 score={:.3f})'.format(score), precision, recall
-
-    # Test the Nu support vector classifier
-    classifier = NuSVC(kernel='rbf', nu=0.5, gamma=1e-3)
-    # Fit the classifier
-    classifier.fit(X_train, y_train)
-    score = f1_score(y_test, classifier.predict(X_test))
-    # Generate the P-R curve
-    y_prob = classifier.decision_function(X_test)
-    precision, recall, _ = precision_recall_curve(y_test, y_prob)
-    # Include the score in the title
-    yield 'NuSVC (F1 score={:.3f})'.format(score), precision, recall
-
-    # Test the Ada boost classifier
-    classifier = AdaBoostClassifier(n_estimators=50, learning_rate=1.0, algorithm='SAMME.R')
-    # Fit the classifier
-    classifier.fit(X_train, y_train)
-    score = f1_score(y_test, classifier.predict(X_test))
-    # Generate the P-R curve
-    y_prob = classifier.decision_function(X_test)
-    precision, recall, _ = precision_recall_curve(y_test, y_prob)
-    # Include the score in the title
-    yield 'Ada Boost (F1 score={:.3f})'.format(score), precision, recall
-
-# =====================================================================
-
-
-def plot(results):
-    '''
-    Create a plot comparing multiple learners.
-
-    `results` is a list of tuples containing:
-        (title, precision, recall)
-    
-    All the elements in results will be plotted.
-    '''
-
-    # Plot the precision-recall curves
-
-    fig = plt.figure(figsize=(12, 6))
-    fig.canvas.set_window_title('Classifying data from ' + URL)
-
-    for label, precision, recall in results:
-        plt.plot(recall, precision, label=label)
-
-    plt.title('Precision-Recall Curves')
-    plt.xlabel('Precision')
-    plt.ylabel('Recall')
-    plt.legend(loc='lower left')
-
-    # Let matplotlib improve the layout
-    plt.tight_layout()
-
-    # ==================================
-    # Display the plot in interactive UI
-    plt.show()
-
-    # To save the plot to an image file, use savefig()
-    #plt.savefig('plot.png')
-
-    # Open the image file with the default image viewer
-    #import subprocess
-    #subprocess.Popen('plot.png', shell=True)
-
-    # To save the plot to an image in memory, use BytesIO and savefig()
-    # This can then be written to any stream-like object, such as a
-    # file or HTTP response.
-    #from io import BytesIO
-    #img_stream = BytesIO()
-    #plt.savefig(img_stream, fmt='png')
-    #img_bytes = img_stream.getvalue()
-    #print('Image is {} bytes - {!r}'.format(len(img_bytes), img_bytes[:8] + b'...'))
-
-    # Closing the figure allows matplotlib to release the memory used.
-    plt.close()
 
 #plots features per file against time
 def plotGraph(dataIn, maxY, fileNameIn, fileAddendum, outputDirectory):
@@ -277,7 +55,7 @@ def plotGraph(dataIn, maxY, fileNameIn, fileAddendum, outputDirectory):
     #fig.canvas.set_window_title('Reading info from excel file')
     for i in range(1,dataIn.shape[1]):
         plt.plot(dataIn[:,0], dataIn[:,i], label='id %s' %i)
-    plt.title(fileNameIn+" Limit: 500")
+    plt.title(fileNameIn+fileAddendum)
     plt.xlabel('Time')
     plt.ylabel('Feature')
     plt.legend()
@@ -288,26 +66,26 @@ def plotGraph(dataIn, maxY, fileNameIn, fileAddendum, outputDirectory):
 
     plt.close()
 
+#Need to edit to make the labels current, otherwise will plot data in a 2d array
+def plotFeature(dataIn, maxY, fileNameIn, fileAddendum, outputDirectory, col):
+
+    fig = plt.figure(figsize=(30, 20))
+    for line in dataIn:
+        plt.plot(line[:,0], line[:,1], label='id %s' %fileNameIn)
+    plt.title(fileNameIn+" Limit: 500")
+    plt.xlabel('Time')
+    plt.ylabel(labels[col])
+    plt.legend()
+    plt.ylim(0, maxY)
+    plt.savefig(outputDirectory+fileNameIn+fileAddendum)
+    #plt.show()
+
+    plt.close()
+
 def plotDirectory(dataIn, maxY, filePaths, plotDescription, outputDirectory):
     for i in range(0,len(dataIn)):
         plotGraph(dataIn[i], maxY, extractFileName(filePaths[i]), plotDescription, outputDirectory)
 
-def plotAxis(xAxis, yAxis, filename):
-
-    fig = plt.figure(figsize=(30, 6))
-    #fig.canvas.set_window_title('Reading info from excel file')
-    for i in range(1,yAxis.shape[0]):
-        plt.plot(xAxis, yAxis[i], label='id %s' %i)
-    plt.title(fileNameIn+" Limit: 500")
-    plt.xlabel('Time')
-    plt.ylabel('Feature')
-    plt.legend()
-    plt.xlim(dataIn[0,0],dataIn[dataIn.shape[0]-1,0])
-    plt.ylim(0,500)
-    plt.savefig("C:\\Users\\jacob\\Desktop\\MachineLearning\\GraphsRawDataLeftRightLimit500\\"+fileNameIn+"Limit500")
-    #plt.show()
-
-    plt.close()
 
 # ======================String Stuff==================================
 
@@ -325,12 +103,33 @@ def extractSubstring(stringIn, Delimiter):
 def extractFileName(stringIn):
     return extractSubstring(rExtractSubstring(stringIn, "\\"), ".")
 
+# ======================  Re-scaling  ==================================
+
+#rescales a single data point
+def rescale(x, min, max):
+    range = max - min
+    return ((x-min)/range)
+
+#used for rescaling a single numpy array
+def rescaleX(dataIn):
+    max = dataIn[dataIn.shape[0]-1][0]
+    min = dataIn[0][0]
+    for i in range(0, len(dataIn)):
+       dataIn[i,0] = rescale(dataIn[i][0], min, max)   
+    return dataIn
+
+#Used for rescaling an array of numpy arrays
+def rescaleSet(dataIn):
+    for i in range(0, len(dataIn)):
+        data[i] = rescaleX(dataIn[i])
+    return dataIn
+
 # =========================  Reading  ============================================
-#Reads in features by file
+#Reads in features by file - Currently not working - need to refactor to return data instead of graphing it
 def ReadByFile(filePaths, rowsToSkipUse, colsToUse):
     for file in filePaths:
             data = np.loadtxt(file, delimiter=",", skiprows=1, usecols = colsToUse)
-            plotTest(data, extractFileName(file))
+            plotGraph(data, 500, extractFileName(file))
 
 def ReadByFileRate(filePaths, rowsToSkip, colsToUse, rate):
     data = []
@@ -339,6 +138,7 @@ def ReadByFileRate(filePaths, rowsToSkip, colsToUse, rate):
     finalData = np.asarray(data)
     return data
 
+#Reads in the lines of a file, only reading in the only nth file where n is the passed-in rate
 def ReadFile(filename, skipRows, colsToUse, rate):
     i = 0
     data = []
@@ -350,16 +150,6 @@ def ReadFile(filename, skipRows, colsToUse, rate):
         finalData = np.genfromtxt(data, delimiter=",", skip_header=skipRows, usecols = colsToUse)
         return finalData
 
-
-#Reads in a single column from all files
-def ReadColumn(filePaths, colToRead):
-    column =[]
-    for file in filePaths:
-            data = np.loadtxt(file, delimiter=",", skiprows=1, usecols = colToRead)
-            column = np.append(column, data)
-    finalColumn = np.asarray(column)
-    return finalColumn
-
 #collects all of the filenames in a given directory
 def ReadFilePaths(directory):
     fileNames = []
@@ -369,32 +159,15 @@ def ReadFilePaths(directory):
             fileNames.append(file)
     return fileNames
 
-
-#==================================== Read By Column ================================================
-def ReadColumnByFile(filePaths, colToRead):
-    columns = []
-    for file in filePaths:
-            data = np.loadtxt(file, delimiter=",", skiprows=1, usecols = colToRead)
-            columns.append(data)
-    finalColumns = np.asarray(columns)
-    return finalColumns
-
-def LoadDataByColumn(directory, columnsToRead, xAxis):
-    data = []
-    for col in columnsToRead:
-        data.append(ReadColumnByFile(directory, col))
-        plotAxis(xAxis, ReadColumnByFile(directory, col),"feature")
-    finalData = np.asarray(data)
-    return finalData
-
 if __name__ == '__main__':
-
+    global labels
+    labels = [ "Time (Milliseconds)", "Time" ,"P1", "P2", "P3", "P4", "P5", "P6", "P7", "P8", "P9"]
     directory = 'C:\\Users\\jacob\\Desktop\\MachineLearning\\rawData\\'
     outputDirectory = 'C:\\Users\\jacob\\Desktop\\MachineLearning\\test\\'
     paths = ReadFilePaths(directory)
-    #ReadByFile(directory,1,(0, 2, 3, 4, 5, 6, 7, 8, 9, 10))
-    data = ReadByFileRate(paths,1,(0, 2, 3, 4, 5, 6, 7, 8, 9, 10), 20)
-    plotDirectory(data, 500, paths, "max500", outputDirectory)
-
-    #xAxis = ReadColumn(directory, 0)
-    #yAxis = LoadDataByColumn(directory, (2, 3, 4, 5, 6, 7, 8, 9, 10), xAxis)
+    #data = ReadByFileRate(paths,1,(0, 2, 3, 4, 5, 6, 7, 8, 9, 10), 20)
+    #plotDirectory(data, 500, paths, "max500", outputDirectory)
+    col = 2
+    data = ReadByFileRate(paths,1,(0, 2), 40)
+    data = rescaleSet(data)
+    plotFeature(data, 500, labels[col], "by Feature", outputDirectory, col) #Works finally - looks awful, will need to pass in selected files
