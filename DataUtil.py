7f508eb124dc5b03ce294f1d31ae60ab7925d310
dkStephanos <stephanos@goldmail.etsu.edu>
2020-03-30 13:41:32 -0400

    Added cleaning methods to remove rows with questionable data

--- DataUtil.py
+++ DataUtil.py
@@ -27,26 +27,61 @@ class DataUtil:
             dataIn[i] = DataUtil.rescaleX(dataIn[i])
         return dataIn
 
+    @staticmethod
+    def trimOutliersBySTD(dataFrame):
+        factor = 4
+        for col in dataFrame:
+            upper_lim = dataFrame[col].mean () + dataFrame[col].std () * factor
+            lower_lim = dataFrame[col].mean () - dataFrame[col].std () * factor
+            dataFrame = dataFrame[(dataFrame[col] < upper_lim) & (dataFrame[col] > lower_lim)]
+
+        return dataFrame
+
+    @staticmethod
+    def trimOutliersBySTD(dataFrame, factor):
+        for col in dataFrame.iloc[:,1:]:
+            upper_lim = dataFrame[col].mean () + dataFrame[col].std () * factor
+            lower_lim = dataFrame[col].mean () - dataFrame[col].std () * factor
+            dataFrame = dataFrame[(dataFrame[col] < upper_lim) & (dataFrame[col] > lower_lim)]
+
+        return dataFrame
+
+    @staticmethod
+    def trimRowsWithZeroValues(dataFrame):
+        for col in dataFrame:
+            dataFrame = dataFrame[(dataFrame[col] != 0.0)]
+
+        return dataFrame
+
+    @staticmethod
+    def cleanRows(dataFrame, factor):
+        dataFrame = DataUtil.trimRowsWithZeroValues(dataFrame)
+        dataFrame = DataUtil.trimOutliersBySTD(dataFrame, factor)
+
+        return dataFrame
+
     #Builds a labled pandas dataframe out of multidimesnional numpy array
     def shoeDataToDataFrame(data):
         labels = [ "Time (Milliseconds)","P1", "P2", "P3", "P4", "P5", "P6", "P7", "P8", "acX", "acY", "acZ"]
         dataFrames = list()
         for d in data:
             d = pd.DataFrame(d, columns=labels)
+            print("Starting length: {0}\n".format(len(d)))
+            d = DataUtil.cleanRows(d, 4)
+            print("Ending length: {0}\n".format(len(d)))
             dataFrames.append(d)
 
         return dataFrames
 
     #Builds a labled pandas dataframe out of multidimesnional numpy array
     def phoneDataToDataFrame(data):
-        accLabels = [ "Time (Milliseconds)","ax", "ay", "az"]
-        gyroLabels = [ "Time (Milliseconds)","gx", "gy", "gz"]
+        labels = [[ "Time (Milliseconds)","ax", "ay", "az"], [ "Time (Milliseconds)","gx", "gy", "gz"]]
         dataFrames = list()
 
-        data[0] = pd.DataFrame(data[0], columns=accLabels)
-        dataFrames.append(data[0])
-        data[1] = pd.DataFrame(data[1], columns=gyroLabels)
-        dataFrames.append(data[1])
+        for i in range(0,2):
+            data[i] = pd.DataFrame(data[i], columns=labels[i])
+            data[i] = DataUtil.cleanRows(data[i], 4)
+            dataFrames.append(data[i])
 
         return dataFrames
 
